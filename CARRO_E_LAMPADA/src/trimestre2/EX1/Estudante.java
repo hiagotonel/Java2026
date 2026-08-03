7bb3c248771978e2a6798acbad0adffa72d1c788
hiagotonel <hiagotonel@gmail.com>
2026-08-03 16:23:19 -0300

    Construtor e imports EX1

--- CARRO_E_LAMPADA/src/trimestre2/EX1/Estudante.java
+++ CARRO_E_LAMPADA/src/trimestre2/EX1/Estudante.java
@@ -1,5 +1,13 @@
 package trimestre2.EX1;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Estudante {
-    
+    private String nome;
+    private List<Double> notas;
+
+    public Estudante(String nome){
+        this.nome = nome;
+        this.notas = new ArrayList<>();
+    }
 }
