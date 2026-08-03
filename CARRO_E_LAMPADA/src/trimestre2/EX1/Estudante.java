89a01337f336e9b84417cdc7bc44f32cbd0b85e8
hiagotonel <hiagotonel@gmail.com>
2026-08-03 16:32:42 -0300

    Método set notas[]

--- CARRO_E_LAMPADA/src/trimestre2/EX1/Estudante.java
+++ CARRO_E_LAMPADA/src/trimestre2/EX1/Estudante.java
@@ -1,13 +1,24 @@
 package trimestre2.EX1;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Estudante {
     private String nome;
     private List<Double> notas;
+    Scanner scanner = new Scanner(System.in);
+    
 
     public Estudante(String nome){
         this.nome = nome;
         this.notas = new ArrayList<>();
+    }
+
+    public void insereNotas(){
+        for(Integer i=0;i<=5;i++){
+            System.out.println("Insira a nota "+(i+1)+" do aluno "+ this.nome +":");
+            Double nota = scanner.nextDouble();
+            this.notas.add(nota);
+        }
     }
 }
