e353f63133fdc04d61971b832e3db4da1901b995
hiagotonel <hiagotonel@gmail.com>
2026-08-10 15:30:56 -0300

    Exibição dos dados no Main

--- Java/src/trimestre2/EX1/Main.java
+++ Java/src/trimestre2/EX1/Main.java
@@ -1,10 +1,15 @@
 package trimestre2.EX1;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String nome = scanner.nextLine();
+        Estudante estudante = new Estudante(nome);
+        estudante.insereNotas();
+        System.out.println(estudante.getNotas());
+        System.out.println("Menor nota do(a) "+estudante.getNome()+": "+estudante.menorNota());
+        System.out.println("Média do(a) "+estudante.getNome()+": "+estudante.calculaMedia());
+        scanner.close();
     }
 }
