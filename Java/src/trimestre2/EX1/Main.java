4a13b65a63b8ab0a2b51c99790a3b7034fb093d9
hiagotonel <hiagotonel@gmail.com>
2026-08-10 15:22:52 -0300

    Scanner pra nome()

--- Java/src/trimestre2/EX1/Main.java
+++ Java/src/trimestre2/EX1/Main.java
@@ -1,10 +1,10 @@
 package trimestre2.EX1;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        
+        Scanner scanner = new Scanner(System.in);
+        String nome = scanner.nextLine();
     }
 }
