3b203b9c14885e4b0e2bea3f6c4c90fe86ffad3b
hiagotonel <hiagotonel@gmail.com>
2026-08-10 21:32:20 -0300

    Laço de repetição pra criar estudantes e começo do uso dos aprovados

--- Java/src/trimestre2/EX2/Main.java
+++ Java/src/trimestre2/EX2/Main.java
@@ -1,16 +1,26 @@
 package trimestre2.EX2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        ArrayList<Estudante> estudantes = null;
         Scanner scanner = new Scanner(System.in);
-        String nome = scanner.nextLine();
-        Estudante estudante = new Estudante(nome);
-        estudante.insereNotas();
+        int opt = 0;
+        do {
+            String nome = scanner.nextLine();
+            estudantes.add(new Estudante(nome));
+            estudantes.getLast().insereNotas();
+            System.out.println("Digite '0' para parar de inserir estudantes: ");
+            opt = scanner.nextInt();
+        } while (opt != 0);
+
+        ArrayList<Estudante> aprovados = estudantes.getFirst().aprovados(estudantes);
+
+
         System.out.println(estudante.getNotas());
-        System.out.println("Menor nota do(a) "+estudante.getNome()+": "+estudante.menorNota());
-        System.out.println("Média do(a) "+estudante.getNome()+": "+estudante.calculaMedia());
-        scanner.close();
+        System.out.println("Menor nota do(a) " + estudante.getNome() + ": " + estudante.menorNota());
+        System.out.println("Média do(a) " + estudante.getNome() + ": " + estudante.calculaMedia());
     }
 }
