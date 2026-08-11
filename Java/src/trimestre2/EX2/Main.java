1a44202ded48432296cab8884b9fd80c8f3a1297
hiagotonel <hiagotonel@gmail.com>
2026-08-10 21:39:28 -0300

    Laço de repetição e verificação para exibição dos dados dos aprovados

--- Java/src/trimestre2/EX2/Main.java
+++ Java/src/trimestre2/EX2/Main.java
@@ -9,6 +9,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int opt = 0;
         do {
+            System.out.println("Insira o nome do aluno: ");
             String nome = scanner.nextLine();
             estudantes.add(new Estudante(nome));
             estudantes.getLast().insereNotas();
@@ -18,9 +19,16 @@ public class Main {
 
         ArrayList<Estudante> aprovados = estudantes.getFirst().aprovados(estudantes);
 
-
-        System.out.println(estudante.getNotas());
-        System.out.println("Menor nota do(a) " + estudante.getNome() + ": " + estudante.menorNota());
-        System.out.println("Média do(a) " + estudante.getNome() + ": " + estudante.calculaMedia());
+        if(aprovados.isEmpty()){
+            System.out.println("Nenhum estudante encontrado");
+        }
+        else{
+            System.out.println("DADOS DOS ESTUDANTES APROVADOS: \n");
+            for(int i=0;i<aprovados.size();i++){
+                System.out.println(aprovados.get(i).getNotas());
+                System.out.println("Menor nota do(a) " + aprovados.get(i).getNome() + ": " + aprovados.get(i).menorNota());
+                System.out.println("Média do(a) " + aprovados.get(i).getNome() + ": " + aprovados.get(i).calculaMedia());
+            }
+        }
     }
 }
