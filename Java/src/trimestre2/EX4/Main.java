049e3d8d4321b07c851a331dbd9e39265b9c063d
hiagotonel <hiagotonel@gmail.com>
2026-08-17 16:12:58 -0300

    arrumando meia coisa

--- Java/src/trimestre2/EX4/Main.java
+++ Java/src/trimestre2/EX4/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
         ArrayList<Estudante> aprovados = Estudante.aprovados(estudantes);
 
-        if(aprovados.isEmpty()){
+        if(aprovados==null){
             System.out.println("Nenhum estudante aprovado");
         }
         else{
