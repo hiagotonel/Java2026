f6024e29f4da0890a145abbc697562bf1d0f405d
hiagotonel <hiagotonel@gmail.com>
2026-08-17 20:20:15 -0300

    get, set, construtor e variaveis do periodico

--- Java/src/trimestre2/EX5/Periodico.java
+++ Java/src/trimestre2/EX5/Periodico.java
@@ -1,4 +1,27 @@
 package trimestre2.EX5;
 
 public class Periodico {
+    private String titulo;
+    private Integer volume;
+
+    public Periodico(String titulo, Integer volume){
+        this.titulo= titulo;
+        this.volume = volume;
+    }
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public Integer getVolume() {
+        return volume;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
 }
