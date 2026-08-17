800d46ffa9be558d480b7d088ce0d88552358757
hiagotonel <hiagotonel@gmail.com>
2026-08-17 20:16:20 -0300

    get, set, construtor e variaveis do livro

--- Java/src/trimestre2/EX5/Livro.java
+++ Java/src/trimestre2/EX5/Livro.java
@@ -1,4 +1,27 @@
 package trimestre2.EX5;
 
 public class Livro {
+    private String titulo;
+    private Boolean disponivel;
+
+    public Livro(String titulo, Boolean disponivel){
+        this.titulo = titulo;
+        this.disponivel = disponivel;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Boolean getDisponivel(){
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
 }
