5bf71d175192b7ef6f2f3c44e0bd58479bb374a4
hiagotonel <hiagotonel@gmail.com>
2026-08-17 20:34:58 -0300

    estrutura base pro main (esqueci de commitar um monte de coisa kkkkkkk)

--- Java/src/trimestre2/EX5/Main.java
+++ Java/src/trimestre2/EX5/Main.java
@@ -2,6 +2,43 @@ package trimestre2.EX5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Funfou");
+
+        Livro livro = new Livro("O Senhor dos Anéis");
+
+        Periodico periodico = new Periodico("Revista Ciência Hoje", 15);
+
+        System.out.println("===== LIVRO =====");
+        System.out.println("Título: " + livro.getTitulo());
+        System.out.println("situação: " + livro.isDisponivel());
+
+        System.out.println();
+
+        System.out.println("===== PERIÓDICO =====");
+        System.out.println("Título: " + periodico.getTitulo());
+        System.out.println("Volume: " + periodico.getVolume());
+
+        System.out.println("\n===== EMPRÉSTIMO =====");
+        livro.emprestar();
+
+        System.out.println("Livro: " + livro.getTitulo());
+        System.out.println("Disponível após empréstimo: " + livro.isDisponivel());
+
+        System.out.println("\nTentando emprestar o livro novamente:");
+        livro.emprestar();
+
+        System.out.println("\n===== DEVOLUÇÃO =====");
+        livro.devolver();
+
+        System.out.println("Livro: " + livro.getTitulo());
+        System.out.println("Disponível após devolução: " + livro.isDisponivel());
+
+        System.out.println("\nTentando devolver o livro novamente:");
+        livro.devolver();
+
+        System.out.println("\n===== PERIÓDICO =====");
+        System.out.println("Título: " + periodico.getTitulo());
+        System.out.println("Volume: " + periodico.getVolume());
+
+        System.out.println("Periódicos não podem ser emprestados.");
     }
 }
